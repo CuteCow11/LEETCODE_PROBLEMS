cde610cdb351a3ea51e9f4e9f386b29ab952da07
CuteCow11 <krishshukla.11052006@gmail.com>
2026-07-15 18:16:25 +0530

    Time: 55 ms (5.12%) | Memory: 47.5 MB (19.01%) - LeetSync

--- 4313-password-strength/password-strength.java
+++ 4313-password-strength/password-strength.java
@@ -8,21 +8,16 @@ class Solution {
             if (c >= 48 && c <= 57) {
                 strg = strg + 3;
                 password = password.replace(String.valueOf(password.charAt(i)), "");
-                i=0;
-
             } else if (c >= 65 && c <= 90) {
                 strg = strg + 2;
                 password = password.replace(String.valueOf(password.charAt(i)), "");
-                i=0;
             } else if (c >= 97 && c <= 122) {
                 strg = strg + 1;
                 password = password.replace(String.valueOf(password.charAt(i)), "");
-                i=0;
 
             } else if (c == 33 || c == 64 || c == 35 || c == 36) {
                 strg = strg + 5;
                 password = password.replace(String.valueOf(password.charAt(i)), "");
-                i=0;
             } else {
                 i++;
                 continue;
